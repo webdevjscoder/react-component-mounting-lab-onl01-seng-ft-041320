fa51db2b33a80e5b55149d25e2eb2e680bce72d7
Juan Cobian <cobianjuan19@gmail.com>
2020-08-20 22:24:18 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -7,7 +7,9 @@ class Timer extends Component {
   };
 
   // add your code here
-  this.interval = setInterval(this.clockTick, 1000)
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
 
   render() {
     const { time, color } = this.state;
